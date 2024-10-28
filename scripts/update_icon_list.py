5a92d151e683cd74216eac0f365ef1a05ad1b2c5
rysson <robert.kalinowski@sharkbits.com>
2024-10-28 10:44:31 +0100

    update icon script

--- scripts/update_icon_list.py
+++ scripts/update_icon_list.py
@@ -1,23 +1,32 @@
 'Update the icon_list module from the FontAwesome GitHub repo'
 import requests
 import json
-import itertools
-import pprint
+from pathlib import Path
+from argparse import ArgumentParser
 
 URI = ('https://raw.githubusercontent.com'
-       '/FortAwesome/Font-Awesome/master/advanced-options/metadata/icons.json')
+       '/FortAwesome/Font-Awesome/refs/heads/{ref}/metadata/icons.json')
 
 
-def main():
-    icons_json = requests.get(URI).json()
-    # use only styles
+def main(argv=None):
+    ap = ArgumentParser()
+    ap.add_argument('ref', nargs='?', default='master', help='AwesomeFont version (master, 5.x, 6.x...)')
+    op = ap.parse_args(argv)
+
+    icons_json = requests.get(URI.format(ref=op.ref)).json()
+
+    # use only styles for names and aliases
     icons = {
-        icon_name: tuple(icons_json[icon_name]['styles'])
-        for icon_name in icons_json.keys()
+        name: tuple(icon['styles'])
+        for icon_name, icon in icons_json.items()
+        for name in (icon_name, *(icon.get('aliases', {}).get('names', ())))
     }
-    with open('../fontawesome_markdown/icon_list.py', 'w') as icons_list_py:
+
+    with open(Path(__file__).parent.parent / 'fontawesome_markdown' / 'icon_list.py', 'w') as icons_list_py:
         icons_list_py.write('from __future__ import unicode_literals\n')
         icons_list_py.write('icons = ')
         icons_list_py.write(json.dumps(icons, indent=2))
+
+
 if __name__ == '__main__':
     main()
